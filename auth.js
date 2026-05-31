c71edfc05b78fb966c197556506d90268941e955
edn869728-jpg <edn869728@gmail.com>
2026-06-01 02:04:19 +0800

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,30 @@
+window.ANG_HR_FEATURES={
+  "clock": 1,
+  "clock_fix": 1,
+  "leave": 1,
+  "salary": 1,
+  "notice": 1,
+  "admin": 1,
+  "review": 1,
+  "people": 1,
+  "schedule": 1,
+  "upload": 1,
+  "admin_schedule": 1,
+  "admin_salary": 1,
+  "settings": 0,
+  "data": 0,
+  "drive": 0,
+  "api": 0,
+  "multi_review": 0,
+  "ios_shortcut": 1,
+  "field_clock": 1,
+  "overtime_clock": 1,
+  "branches": 5,
+  "employees": 25
+};
+window.ANG_HR_PLAN={
+  "edition": "plus",
+  "name": "Plus",
+  "tagline": "標準店家｜多店排班薪資版",
+  "included": "5 店｜25 人"
+};
