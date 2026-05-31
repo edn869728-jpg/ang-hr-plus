c0ca61d37fd0b5c81e1b9dee63b78f449e653973
edn869728-jpg <edn869728@gmail.com>
2026-06-01 00:03:11 +0800

    Add files via upload

---
+++ assets/config.js
@@ -0,0 +1,10 @@
+window.ANG_HR_CONFIG = {
+  edition: 'plus',
+  editionName: 'ANG HR Plus',
+
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbzSwufSpMlK-DdmrxJ6m-jZvUT__iSUsDdCNXQaXUbCScs_-h9MTWP_7grMIq03RzILbQ/exec',
+  gasUrl: 'https://script.google.com/macros/s/AKfycbzSwufSpMlK-DdmrxJ6m-jZvUT__iSUsDdCNXQaXUbCScs_-h9MTWP_7grMIq03RzILbQ/exec',
+
+  defaultPage: 'admin',
+  version: 'leave_calc_v2'
+};
